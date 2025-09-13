df4b4a2aed78a39a829a00b7066c099839d58cb3
pinprosper1 <pinprosper1@gmail.com>
2025-09-13 19:16:06 +0200

    small tablet fixes

--- js/campaign.js
+++ js/campaign.js
@@ -1,7 +1,9 @@
 function showSidebar() {
     document.querySelector('.sidebar').classList.add('open');
+    document.body.style.overflow = 'hidden';
 }
 
 function hideSidebar() {
     document.querySelector('.sidebar').classList.remove('open');
+    document.body.style.overflow = 'auto';
 }
--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 function showSidebar() {
     document.querySelector('.sidebar').classList.add('open');
+    document.body.style.overflow = 'hidden';
 }
 
 function hideSidebar() {
     document.querySelector('.sidebar').classList.remove('open');
+    document.body.style.overflow = 'auto';
 }
 
 document.addEventListener("DOMContentLoaded", () => {
